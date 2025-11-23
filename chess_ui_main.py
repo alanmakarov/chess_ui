49fcf2425f616d6d1920386e2e05599df6272349
Alexey Makarov <amakarov@gmail.com>
2025-11-23 22:09:19 +0300

    add stub

---
+++ chess_ui_main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from gameBoard import I_GameBoard, MocGameBoard
+
+class ChessBoard:
+    def __init__(self) -> None:
+        pass
+    def draw_board(self):
+        pass
+    def on_square_click(self, event):
+        pass
+    def on_button_click(self):
+        pass
+
+    def run(self):
+        self.root.mainloop()
+
+
+chess = ChessBoard()
+chess.run()
